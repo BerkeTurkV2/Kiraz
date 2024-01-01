3deeee129beed080eb03cf5db6d9f660c8f45618
Berke <forcoding2022v2@gmail.com>
2024-01-01 14:48:02 +0300

    Up

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View,Text} from "react-native"
+
+function Router() {
+    return(
+        <View>
+            <Text>Selam</Text>
+        </View>
+    )
+};
+
+export default Router;
